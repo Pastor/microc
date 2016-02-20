6a486824b86d59c9c280003e19d63bc1d9bdfad5
Хлебников Андрей Александрович <a.hlebnikov@voskhod.ru>
2016-02-20 13:41:35 +0300

    Внутренний вызов

--- stam.c
+++ stam.c
@@ -8,7 +8,6 @@
 
 /**
 https://defuse.ca/online-x86-assembler.htm
-
 */
 
 void * 
@@ -20,31 +19,35 @@ alloc_memory(size_t size)
 void 
 emit_code(uint8_t * m) 
 {
-    uint8_t *p = m + 19;
+    uint8_t *p = m + 25;
     uint8_t call[] = { ((uint8_t *)&p)[0], ((uint8_t *)&p)[1], ((uint8_t *)&p)[2], ((uint8_t *)&p)[3] };
     uint8_t code[] = {
         0x55,              // push ebp
         0x89, 0xe5,        // mov  ebp, esp
         0x8b, 0x45, 0x08,  // mov  eax, dword ptr[ebp + 0x08]
         0x83, 0xc0, 0x04,  // add  eax, 0x04
-        0x5d,              // pop  ebp
         0xbb, call[0], call[1], call[2], call[3], //mov ebx, 0x00000000
         0x50,              // push eax
         0xff, 0xd3,        // call ebx
-        0xc3,              // ret
 
+        0x31, 0xdb,        // xor  ebx, ebx
+        0x53,              // push ebx
+        0x5b,              // pop  ebx
 
+        0x5d,              // pop  ebp
+        0xc2, 0x04, 0x00,  // ret  0x04
+        
         0x55,              // push ebp
         0x89, 0xe5,        // mov  ebp, esp
         0x8b, 0x45, 0x08,  // mov  eax, dword ptr[ebp + 0x08]
         0x83, 0xc0, 0x06,  // add  eax, 0x06
         0x5d,              // pop  ebp
-        0xc3               // ret
+        0xc2, 0x04, 0x00   // ret  0x04
     };
     memcpy(m, code, sizeof(code));
 }
 
-typedef long (__cdecl * JittedCall )( long arg );
+typedef long (__stdcall * JittedCall )( long arg );
 
 void 
 start() 
